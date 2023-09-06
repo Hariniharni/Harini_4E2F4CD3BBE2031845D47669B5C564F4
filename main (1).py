a1780d45f4e532041e497072779163480a67e534
Hariniharni <144227089+Hariniharni@users.noreply.github.com>
2023-09-06 16:36:17 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,8 +1,7 @@
-def factorial(n):
-  if(n==0 or n==1):
-    return 1
-  else:
-    return n*factorial(n-1)
-    num=int(input("enter the number to finad factorial:"))
-    res=factorial(num)
-    print("the factorial of {} is {}".format(num,res))
+year=2000
+if(year % 400 ==0) and (year % 100 ==0):
+  print("{0} is a leap yaer".format(year))
+elif(year % 4==0) and (year % 100 !=0):
+   print("{0} is a leap yeae".format(year))
+else:
+   print("{0} is not a leap year".format(year))
